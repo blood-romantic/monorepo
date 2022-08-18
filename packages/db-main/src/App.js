b6157187996e264b5f779a5863585e3fb51e1b08
greg wang <greg.wang@e-hps.com>
2022-08-18 10:38:08 +0800

    initial commit

---
+++ packages/db-main/src/App.js
@@ -0,0 +1,14 @@
+import { Header } from 'db-header';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+          Main content
+      </div>
+    </>
+  );
+}
+
+export default App;
